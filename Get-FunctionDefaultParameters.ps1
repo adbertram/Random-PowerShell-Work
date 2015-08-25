d7928ffe1921ed9c99397c2fcf65172fd27a8ae6
Adam Bertram <adbertram@gmail.com>
2015-08-25 17:03:49 -0500

    added new script

---
+++ Get-FunctionDefaultParameters.ps1
@@ -0,0 +1,14 @@
+﻿function Get-FunctionDefaultParameters
+{
+	[CmdletBinding()]
+	param (
+		[string]$FunctionName
+	)
+	$ast = [System.Management.Automation.Language.Parser]::ParseInput((Get-Command $FunctionName).Definition, [ref]$null, [ref]$null)
+	
+	$charReplace = '{0}|{1}' -f "'", '"'
+	$select = @{ n = 'Name'; e = { $_.Name.VariablePath.UserPath } },
+		@{ n = 'Value'; e = { $_.DefaultValue.Extent.Text -replace $charReplace, '' } }
+	
+	$ast.FindAll({ $args[0] -is [System.Management.Automation.Language.ParameterAst] }, $true) | where { $_.DefaultValue } | select $select
+}
