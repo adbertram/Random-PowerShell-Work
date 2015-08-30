81ec6f130745b608d62079948ef080165d328fcc
Adam Bertram <adbertram@gmail.com>
2015-08-30 12:48:40 -0500

    readme update

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Random-PowerShell-Work
-This is a conglomeration of PowerShell scripts that I've written over the years.
+This is a conglomeration of PowerShell scripts that I've written over the years. If you'd like more PowerShell awesomeness check out my blog at [Adam, the Automator](http://www.adamtheautomator.com).  This is where I write about automation, lots of PowerShell and post regularly about time-saving tricks with PowerShell.
--- Show-SimpleMenu.ps1
+++ Show-SimpleMenu.ps1
@@ -6,14 +6,15 @@
 	cls
 	Write-Host "================ $Title ================"
 	
-	Write-Host "1: Press '1' to begin scanning books."
-	Write-Host "2: Press '2' to change locator code."
-	Write-Host "3: Press '3' to change condition"
+	Write-Host "1: Press '1' for this option."
+	Write-Host "2: Press '2' for this option."
+	Write-Host "3: Press '3' for this option."
 	Write-Host "Q: Press 'Q' to quit."
 }
 
 do
 {
+	Show-Menu
 	$input = Read-Host "Please make a selection"
 	switch ($input)
 	{
