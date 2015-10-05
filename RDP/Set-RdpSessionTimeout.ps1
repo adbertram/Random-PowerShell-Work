09ae2f398b11b13916cebce61f21567a26beac23
Adam Bertram <adbertram@gmail.com>
2015-10-05 11:18:17 -0500

    Added RDP script

---
+++ RDP/Set-RdpSessionTimeout.ps1
@@ -0,0 +1,62 @@
+﻿function Set-RdpSessionTimeout
+{
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Mandatory)]
+		[ValidateNotNullOrEmpty()]
+		[string[]]$ComputerName,
+		
+		[Parameter(Mandatory)]
+		[ValidateNotNullOrEmpty()]
+		[ValidateSet('ActiveButIdle', 'Disconnected', 'Active')]
+		[string]$SessionType,
+		
+		[Parameter(Mandatory)]
+		[ValidateNotNullOrEmpty()]
+		[ValidateRange(1, [int]::MaxValue)]
+		[int]$Timeout, ## in seconds
+		
+		[Parameter()]
+		[ValidateNotNullOrEmpty()]
+		[pscredential]$Credential
+	)
+	try
+	{
+		$baseRegKey = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services'
+		switch ($SessionType)
+		{
+			'ActiveButIdle' {
+				$valueName = 'MaxIdleTime'
+			}
+			'Disconnected' {
+				$valueName = 'MaxDisconnectionTime'
+			}
+			'Active' {
+				$valueName = 'MaxConnectionTime'
+			}
+			default
+			{
+				throw 'Unknown session type chosen.'
+			}
+		}
+		$setParams = @{
+			'Path' = $baseRegKey
+			'Name' = $valueName
+			'Value' = ($Timeout * 1000)
+		}
+		
+		$icmParams = @{
+			'ComputerName' = $ComputerName
+		}
+		if ($PSBoundParameters.ContainsKey('Credential'))
+		{
+			$icmParams.Credential = $Credential
+		}
+		Invoke-Command @icmParams -ScriptBlock { Set-ItemProperty @using:setParams }
+	}
+	catch
+	{
+		$PSCmdlet.ThrowTerminatingError($_)
+	}
+}
