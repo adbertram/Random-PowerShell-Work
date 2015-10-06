e7ec46f92c5737ed837b808f3a87ad59d252b6a3
Adam Bertram <adbertram@gmail.com>
2015-10-06 15:26:47 -0500

    updated to import-certificate.ps1

--- Certificates/Import-Certificate.ps1
+++ Certificates/Import-Certificate.ps1
@@ -1,69 +1,120 @@
-﻿<#
-.SYNOPSIS
-	This script imports a certificate into any certificate store on a local computer
-.NOTES
-	Created on: 	8/6/2014
-	Created by: 	Adam Bertram
-	Filename:		Import-Certificate.ps1
-.EXAMPLE
-	PS> .\Import-Certificate.ps1 -Location LocalMachine -StoreName My -FilePath C:\certificate.cer
+﻿function Import-Certificate
+{
+	<#
+	.SYNOPSIS
+		This function imports a certificate into any certificate store on a local computer
+	.EXAMPLE
+		PS> Import-Certificate -Location LocalMachine -StoreName My -FilePath C:\certificate.cer
 
-	This example will import the certificate.cert certificate into the Personal store for the 
-	local computer
-.EXAMPLE
-	PS> .\Import-Certificate.ps1 -Location CurrentUser -StoreName TrustedPublisher -FilePath C:\certificate.cer
+		This example will import the certificate.cert certificate into the Personal store for the 
+		local computer
+	.EXAMPLE
+		PS> Import-Certificate -Location CurrentUser -StoreName TrustedPublisher -FilePath C:\certificate.cer
 
-	This example will import the certificate.cer certificate into the Trusted Publishers store for the 
-	currently logged on user
-.PARAMETER Location
- 	This is the location (either CurrentUser or LocalMachine) where the store is located which the certificate
-	will go into
-.PARAMETER StoreName
-	This is the certificate store that the certificate will be placed into
-.PARAMETER FilePath
-	This is the path to the certificate file that you'd like to import
-#>
-[CmdletBinding()]
-[OutputType()]
-param (
-	[Parameter(Mandatory)]
-	[ValidateSet('CurrentUser', 'LocalMachine')]
-	[string]$Location,
-	[Parameter(Mandatory)]
-	[ValidateScript({
-		if ($Location -eq 'CurrentUser') {
-			(Get-ChildItem Cert:\CurrentUser | select -ExpandProperty name) -contains $_
-		} else {
-			(Get-ChildItem Cert:\LocalMachine | select -ExpandProperty name) -contains $_
+		This example will import the certificate.cer certificate into the Trusted Publishers store for the 
+		currently logged on user
+	.PARAMETER Location
+	 	This is the location (either CurrentUser or LocalMachine) where the store is located which the certificate
+		will go into
+	.PARAMETER StoreName
+		This is the certificate store that the certificate will be placed into
+	.PARAMETER FilePath
+		This is the path to the certificate file that you'd like to import
+	#>
+	[CmdletBinding()]
+	param
+	(
+		[Parameter()]
+		[ValidateNotNullOrEmpty()]
+		[string]$ComputerName = $env:COMPUTERNAME,
+		
+		[Parameter()]
+		[ValidateNotNullOrEmpty()]
+		[pscredential]$Credential,
+		
+		[Parameter(Mandatory)]
+		[ValidateNotNullOrEmpty()]
+		[ValidateSet('CurrentUser', 'LocalMachine')]
+		[string]$Location,
+		
+		[Parameter(Mandatory)]
+		[ValidateNotNullOrEmpty()]
+		[ValidateScript({
+			if ($Location -eq 'CurrentUser')
+			{
+				(Get-ChildItem Cert:\CurrentUser | select -ExpandProperty name) -contains $_
+			}
+			else
+			{
+				(Get-ChildItem Cert:\LocalMachine | select -ExpandProperty name) -contains $_
+			}
+		})]
+		[string]$StoreName,
+		
+		[Parameter(Mandatory)]
+		[string]$FilePath
+	)
+	begin
+	{
+		try
+		{
+			[void][System.Reflection.Assembly]::LoadWithPartialName('System.Security')
 		}
-	})]
-	[string]$StoreName,
-	[Parameter(Mandatory)]
-	[ValidateScript({Test-Path $_ -PathType Leaf })]
-	[string]$FilePath
-)
-
-begin {
-	$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
-	Set-StrictMode -Version Latest
-	try {
-		[void][System.Reflection.Assembly]::LoadWithPartialName("System.Security")
-	} catch {
-		Write-Error $_.Exception.Message
+		catch
+		{
+			$PSCmdlet.ThrowTerminatingError($_)
+		}
 	}
-}
-
-process {
-	try {
-		$Cert = Get-Item $FilePath
-		$Cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2 $Cert
-		foreach ($Store in $StoreName) {
-			$X509Store = New-Object System.Security.Cryptography.X509Certificates.X509Store $Store, $Location
-			$X509Store.Open([System.Security.Cryptography.X509Certificates.OpenFlags]::ReadWrite)
-			$X509Store.Add($Cert)
-			$X509Store.Close()
+	process
+	{
+		try
+		{
+			$sb = {
+				param (
+					[Parameter()]
+					[string]$FilePath,
+					
+					[Parameter()]
+					[string]$Location,
+					
+					[Parameter()]
+					[string]$StoreName
+				)
+				$cert = Get-Item $FilePath
+				$cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2 $cert
+				foreach ($s in $StoreName)
+				{
+					$x509Store = New-Object System.Security.Cryptography.X509Certificates.X509Store $s, $Location
+					$x509Store.Open([System.Security.Cryptography.X509Certificates.OpenFlags]::ReadWrite)
+					$x509Store.Add($cert)
+					$x509Store.Close()
+				}
+			}
+			$sbArgs = $FilePath, $Location, $StoreName
+			if (Test-LocalComputer -ComputerName $ComputerName)
+			{
+				Invoke-Command -ScriptBlock $sb -Args $sbArgs
+			}
+			else
+			{
+				$connParams = @{
+					'ComputerName' = $ComputerName
+				}
+				if ($PSBoundParameters.ContainsKey('Credential'))
+				{
+					$connParams.Credential = $Credential
+				}
+				$session = New-PSSession @connParams
+				$remoteFile = Send-File -Session $session -Path $FilePath -Destination 'C:\'
+				$FilePath = "C:\$([System.IO.Path]::GetFileName($FilePath))"
+				$sbArgs[0] = $FilePath
+				Invoke-Command -Session $session -ScriptBlock $sb -Args $sbArgs
+				Invoke-Command -Session $session -ScriptBlock { Remove-Item -Path $using:FilePath -Force }
+			}
 		}
-	} catch {
-		Write-Error $_.Exception.Message
+		catch
+		{
+			$PSCmdlet.ThrowTerminatingError($_)
+		}
 	}
 }
