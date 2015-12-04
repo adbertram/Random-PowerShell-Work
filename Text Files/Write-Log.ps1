a514ad8a6435fbd495b110b33a14f422d4961fc6
Adam Bertram <adbertram@gmail.com>
2015-12-03 18:25:57 -0600

    added write-log script

---
+++ Text Files/Write-Log.ps1
@@ -0,0 +1,60 @@
+﻿function Write-Log
+{
+	<#
+	.SYNOPSIS
+		This function creates or appends a line to a log file
+
+	.DESCRIPTION
+		This function writes a log line to a log file in the form synonymous with 
+		ConfigMgr logs so that tools such as CMtrace and SMStrace can easily parse 
+		the log file.  It uses the ConfigMgr client log format's file section
+		to add the line of the script in which it was called.
+
+	.PARAMETER  Message
+		The message parameter is the log message you'd like to record to the log file
+
+	.PARAMETER  LogLevel
+		The logging level is the severity rating for the message you're recording. Like ConfigMgr
+		clients, you have 3 severity levels available; 1, 2 and 3 from informational messages
+		for FYI to critical messages that stop the install. This defaults to 1.
+
+	.EXAMPLE
+		PS C:\> Write-Log -Message 'Value1' -LogLevel 'Value2'
+		This example shows how to call the Write-Log function with named parameters.
+
+	.NOTES
+
+	#>
+	[CmdletBinding()]
+	param (
+		[Parameter(
+				   Mandatory = $true)]
+		[string]$Message,
+		
+		[Parameter()]
+		[ValidateSet(1, 2, 3)]
+		[int]$LogLevel = 1
+	)
+	
+	try
+	{
+		$TimeGenerated = "$(Get-Date -Format HH:mm:ss).$((Get-Date).Millisecond)+000"
+		## Build the line which will be recorded to the log file
+		$Line = '<![LOG[{0}]LOG]!><time="{1}" date="{2}" component="{3}" context="" type="{4}" thread="" file="">'
+		$LineFormat = $Message, $TimeGenerated, (Get-Date -Format MM-dd-yyyy), "$($MyInvocation.ScriptName | Split-Path -Leaf):$($MyInvocation.ScriptLineNumber)", $LogLevel
+		$Line = $Line -f $LineFormat
+		
+		if (-not (Test-Path Variable:\ScriptLogFilePath))
+		{
+			Write-Verbose $Message
+		}
+		else
+		{
+			Add-Content -Value $Line -Path $ScriptLogFilePath
+		}
+	}
+	catch
+	{
+		Write-Error $_.Exception.Message
+	}
+}
