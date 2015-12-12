f5312572f128439b07c5af2a0d93560e718e36ec
Adam Bertram <adbertram@gmail.com>
2015-12-12 15:20:29 -0600

    new script

---
+++ Hyper-V/Get-HyperVUptime.ps1
@@ -0,0 +1,12 @@
+﻿$hyperVHost = 'HYPERVSRV'
+$vmUptimes = Get-VM -ComputerName $hyperVHost | select name, uptime, @{ n = 'Type'; e= { 'VM' } }
+
+$wmiParams = @{
+	'ComputerName' = $hyperVHost
+	'Class' = 'Win32_OperatingSystem'
+}
+$hostUptime = Get-WmiObject @wmiParams | select @{ n = 'Uptime'; e = { (Get-Date) - ($_.ConvertToDateTime($_.LastBootUpTime)) } },
+												@{ n = 'Name'; e = { $_.PSComputerName } },
+												@{ n = 'Type'; e = { 'Host' } }
+
+($vmUptimes + $hostUptime)
