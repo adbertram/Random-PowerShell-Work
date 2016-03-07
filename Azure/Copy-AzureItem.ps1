3230a9af0e3620389ee5363a4bd866b5c68d8ab8
Adam Bertram <adbertram@gmail.com>
2016-03-07 12:46:52 -0600

    update

--- Azure/Copy-AzureItem.ps1
+++ Azure/Copy-AzureItem.ps1
@@ -1,28 +1,27 @@
-﻿#region function Copy-AzureItem
-function Copy-AzureItem
+﻿function Copy-AzureItem
 {
 	<#
 	.SYNOPSIS
 		This function simplifies the process of uploading files to an Azure storage account. In order for this function to work you
 		must have already logged into your Azure subscription with Login-AzureAccount. The file uploaded will be called the file
 		name as the storage blob.
-	
-	.EXAMPLE
-		PS> Get-ChildItem -Path C:\Folder | Copy-AzureItem -ContainerName mycontainer -ResourceGroupName myresources -StorageAccountName mysa
 		
-		This example would upload all files in the C:\Folder directory to the container named mycontainer in your Azure storage account.
-	
 	.PARAMETER FilePath
 		The local path of the file(s) you'd like to upload to an Azure storage account container.
 	
 	.PARAMETER ContainerName
 		The name of the Azure storage account container the file will be placed in.
 	
+	.PARAMETER DestinationName
+		The name of the file stored as an Azure blob. By default, it will be the same name as as the local file. Use this parameter
+		to give it a different name once uploaded.
+	
+	.PARAMETER BlobType
+		The type of blob you'd like the file to become when it gets uploaded to the storage account. For example, when
+		uploading VHDs, you should only use Page.
+	
 	.PARAMETER ResourceGroupName
 		The name of the resource group the storage account is in.
-        
-    .PARAMETER BlobType
-        The blob type to create when the file gets to Azure.
 	
 	.PARAMETER StorageAccountName
 		The name of the storage account the container that will hold the file is in.
@@ -42,17 +41,25 @@ function Copy-AzureItem
 		
 		[Parameter()]
 		[ValidateNotNullOrEmpty()]
+		[string]$DestinationName,
+		
+		[Parameter()]
+		[ValidateNotNullOrEmpty()]
 		[ValidateSet('Page', 'Block')]
 		[string]$BlobType = 'Page',
 		
-		[Parameter(Mandatory)]
+		[Parameter()]
 		[ValidateNotNullOrEmpty()]
-		[string]$ResourceGroupName,
+		[string]$ResourceGroupName = 'IT-DevOps',
 		
-		[Parameter(Mandatory)]
+		[Parameter()]
 		[ValidateNotNullOrEmpty()]
-		[string]$StorageAccountName
+		[string]$StorageAccountName = 'pinf01stg02'
 	)
+	begin
+	{
+		$ErrorActionPreference = 'Stop'
+	}
 	process
 	{
 		try
@@ -68,8 +75,17 @@ function Copy-AzureItem
 			
 			$bcParams = @{
 				'File' = $FilePath
-				'Blob' = ($FilePath | Split-Path -Leaf)
+				'BlobType' = $BlobType
 			}
+			if ($PSBoundParameters.ContainsKey('DestinationName'))
+			{
+				$bcParams.Blob = $DestinationName
+			}
+			else
+			{
+				$bcParams.Blob = ($FilePath | Split-Path -Leaf)
+			}
+			
 			Get-AzureRmStorageAccount @saParams | Get-AzureStorageContainer @scParams | Set-AzureStorageBlobContent @bcParams
 		}
 		catch
@@ -78,4 +94,3 @@ function Copy-AzureItem
 		}
 	}
 }
-#endregion function Copy-AzureItem
