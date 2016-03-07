711c4cac12d95d2d7b247c447d79909841188515
Adam Bertram <adbertram@gmail.com>
2016-03-07 12:45:54 -0600

    update

---
+++ Random Stuff/ScriptSharing.psm1
@@ -0,0 +1,188 @@
+﻿#requires -Version 4
+
+Set-StrictMode -Version	Latest
+
+function Send-ToGithub
+{
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Mandatory)]
+		[ValidateNotNullOrEmpty()]
+		[string]$RepositoryName
+			
+	)
+	begin {
+		$ErrorActionPreference = 'Stop'
+	}
+	process {
+		
+	}
+}
+
+function Set-GithubScript
+{
+	[CmdletBinding()]
+	param
+	(
+		
+	)
+	begin {
+		$ErrorActionPreference = 'Stop'
+	}
+	process {
+		
+	}
+}
+
+function Send-ToTechnetScriptRepository
+{
+	[CmdletBinding()]
+	param
+	(
+		
+	)
+	begin {
+		$ErrorActionPreference = 'Stop'
+	}
+	process {
+		
+	}
+}
+
+function Set-TechnetScriptRepositoryScript
+{
+	[CmdletBinding()]
+	param
+	(
+		
+	)
+	begin {
+		$ErrorActionPreference = 'Stop'
+	}
+	process {
+		try
+		{
+				
+		}
+		catch
+		{
+			Write-Error $_.Exception.Message
+		}
+	}
+}
+
+function Send-ToBlog
+{
+	[CmdletBinding()]
+	param
+	(
+		
+	)
+	begin {
+		$ErrorActionPreference = 'Stop'
+	}
+	process {
+		
+	}
+}
+
+function Set-BlogScript
+{
+	[CmdletBinding()]
+	param
+	(
+		
+	)
+	begin {
+		$ErrorActionPreference = 'Stop'
+	}
+	process {
+		
+	}
+}
+
+function Send-ToPoshCode
+{
+	[CmdletBinding()]
+	param
+	(
+		
+	)
+	begin {
+		$ErrorActionPreference = 'Stop'
+	}
+	process {
+		
+	}
+}
+
+function Set-PoshCodeScript
+{
+	[CmdletBinding()]
+	param
+	(
+		
+	)
+	begin {
+		$ErrorActionPreference = 'Stop'
+	}
+	process {
+		
+	}
+}
+
+function Send-ToPowerShellGallery
+{
+	[CmdletBinding()]
+	param
+	(
+		
+	)
+	begin {
+		$ErrorActionPreference = 'Stop'
+	}
+	process {
+		
+	}
+}
+
+function Set-PowerShellGalleryScript
+{
+	[CmdletBinding()]
+	param
+	(
+		
+	)
+	begin {
+		$ErrorActionPreference = 'Stop'
+	}
+	process {
+		
+	}
+}
+
+function Send-ScriptToCommunity
+{
+	[CmdletBinding()]
+	param
+	(
+		[Parameter()]
+		[ValidateNotNullOrEmpty()]
+		[string[]]$Service = @('GitHub','TechnetScriptRepository','PoshCode','Blog')
+			
+	)
+	begin {
+		$ErrorActionPreference = 'Stop'
+	}
+	process {
+		try
+		{
+				
+		}
+		catch
+		{
+			Write-Error $_.Exception.Message
+		}
+	}
+}
