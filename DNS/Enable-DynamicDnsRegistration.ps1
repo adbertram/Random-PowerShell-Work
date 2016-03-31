c56339ca841bffe4da0da7d90b5346073d4b45a1
Adam Bertram <adbertram@gmail.com>
2016-03-31 10:26:26 -0500

    new script

---
+++ DNS/Enable-DynamicDnsRegistration.ps1
@@ -0,0 +1,36 @@
+﻿function Enable-DynamicDnsRegistration
+{
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Mandatory)]
+		[ValidateNotNullOrEmpty()]
+		[string]$ComputerName,
+		
+		[Parameter()]
+		[ValidateNotNullOrEmpty()]
+		[pscredential]$Credential
+	)
+
+	$icmParams = @{
+		'ComputerName' = $ComputerName
+	}
+	if ($PSBoundParameters.ContainsKey('Credential'))
+	{
+		$icmParams.Credential = $Credential
+	}
+	
+	$icmParams.ScriptBlock = {
+		$VerbosePreference = $using:VerbosePreference
+		Get-WmiObject -Class Win32_NetworkAdapterConfiguration -Filter "IPEnabled='true'" | Where-Object { $_.DefaultIPGateway.Count -gt 0 } | ForEach-Object {
+			Write-Verbose -Message "Setting DDNS registration..."
+			$result = $_.SetDynamicDNSRegistration($true)
+			if ($result.ReturnValue -ne 0)
+			{
+				throw "Failed to set DDNS registration on with return code [$($result.ReturnValue)]"
+			}
+		}
+	}
+	
+	Invoke-Command @icmParams
+}
