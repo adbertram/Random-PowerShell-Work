a78ffcc6b4fde22c09e83b6150847d5c6727a922
Adam Bertram <adbertram@gmail.com>
2016-07-31 12:18:42 -0500

    Added new script: Get-DisabledGpo

---
+++ ActiveDirectory/Get-DisabledGpo.ps1
@@ -0,0 +1,42 @@
+﻿#Requires -Version 4 -Module ActiveDirectory
+
+function Get-DisabledGpo
+{
+	<#
+	.SYNOPSIS
+		This function queries the Active Directory domain the initiaing computer is in for all GPOs that either have
+		their computer, user or both settings disabled. This is common when atatempting to find GPOs that can be removed.
+	
+	.EXAMPLE
+		PS> Get-DisabledGpo
+	
+		Name           DisabledSettingsCategory
+		----           ------------------------
+		GPO1        	AllSetting
+		GPO1     		ComputerSetting
+		GPO3 			UserSetting
+	#>
+	[OutputType([pscustomobject])]
+	[CmdletBinding()]
+	param ()
+	begin
+	{
+		$ErrorActionPreference = 'Stop'
+	}
+	process
+	{
+		try
+		{
+			@(Get-GPO -All).where({ $_.GpoStatus -like '*Disabled' }).foreach({
+					[pscustomobject]@{
+						Name = $_.DisplayName
+						DisabledSettingsCategory = ([string]$_.GpoStatus).TrimEnd('Disabled')
+					}	
+				})
+		}
+		catch
+		{
+			$PSCmdlet.ThrowTerminatingError($_)
+		}
+	}
+}
