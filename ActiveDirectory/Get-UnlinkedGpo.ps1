0f32f60378813554c4800f9b100658d64d5d8a78
Adam Bertram <adbertram@gmail.com>
2016-07-31 12:07:13 -0500

    Added Get-UnlinkedGpo

---
+++ ActiveDirectory/Get-UnlinkedGpo.ps1
@@ -0,0 +1,42 @@
+﻿#Requires -Version 4 -Module ActiveDirectory
+
+function Get-UnlinkedGpo
+{
+	<#
+	.SYNOPSIS
+		This function queries the Active Directory domain the initiaing computer is in for all GPOs that do not have a 
+		link to an object. This is common when atatempting to find GPOs that can be removed.
+	
+	.EXAMPLE
+		PS> Get-UnlinkedGpo
+	
+		Comment-Example
+		
+	.PARAMETER Param1
+		A mandatory  paramType1 parameter representing ...
+	
+	#>
+	[CmdletBinding()]
+	param
+	(
+		
+	)
+	begin
+	{
+		$ErrorActionPreference = 'Stop'
+	}
+	process
+	{
+		try
+		{
+			$gpoReport = [xml](Get-GPOReport -All -ReportType XML)
+			@($gpoReport.GPOs.GPO).where({ -not $_.LinksTo }).foreach({
+					[pscustomobject]@{ Name = $_.Name }	
+				})
+		}
+		catch
+		{
+			$PSCmdlet.ThrowTerminatingError($_)
+		}
+	}
+}
