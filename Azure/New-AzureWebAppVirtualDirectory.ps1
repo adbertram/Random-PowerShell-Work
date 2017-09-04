d10bf707e3b11ce812750aed79d98c04bc96d2e1
Adam Bertram <adbertram@gmail.com>
2017-09-04 08:36:23 -0500

    Added new azure script

---
+++ Azure/New-AzureWebAppVirtualDirectory.ps1
@@ -0,0 +1,40 @@
+#requires -Module AzureRm
+
+param(
+	[Parameter(Mandatory)]
+	[ValidateNotNullOrEmpty()]
+	[string]$AzureWebAppName,
+
+	[Parameter(Mandatory)]
+	[ValidateNotNullOrEmpty()]
+	[string]$AzureResourceGroup,
+
+	[Parameter(Mandatory)]
+	[ValidateNotNullOrEmpty()]
+	[string]$VirtualPath,
+
+	[Parameter(Mandatory)]
+	[ValidateNotNullOrEmpty()]
+	[string]$PhysicalPath,
+
+	[Parameter()]
+	[ValidateNotNullOrEmpty()]
+	[bool]$PreloadEnabled = $false
+)
+
+Login-AzurerRmAccount
+
+$webApp = Get-AzureRmWebApp -Name $AzureWebAppName -ResourceGroupName $AzureResourceGroup
+
+$VirtualPath = $VirtualPath.Replace('\', '/')
+$PhysicalPath = "site\$PhysicalPath"
+
+$virtApp = New-Object Microsoft.Azure.Management.WebSites.Models.VirtualApplication
+$virtApp.VirtualPath = $VirtualPath
+$virtApp.PhysicalPath = $PhysicalPath
+$virtApp.PreloadEnabled = $PreloadEnabled
+
+$null = $webApp.siteconfig.VirtualApplications.Add($virtApp)
+
+Set-AzureRmWebApp -WebApp $webApp
+
