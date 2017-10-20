fd81a4012c41550460f95d76151dc0274236ab74
Adam Bertram <adbertram@gmail.com>
2017-10-20 15:25:53 -0500

    Invoke-Process updates

--- Processes/Invoke-Process.ps1
+++ Processes/Invoke-Process.ps1
@@ -1,3 +1,31 @@
+<#PSScriptInfo
+
+.VERSION 1.4
+
+.GUID b787dc5d-8d11-45e9-aeef-5cf3a1f690de
+
+.AUTHOR Adam Bertram
+
+.COMPANYNAME Adam the Automator, LLC
+
+.TAGS Processes
+
+#>
+
+<# 
+
+.DESCRIPTION 
+ 	Invoke-Process is a simple wrapper function that aims to "PowerShellyify" launching typical external processes. There
+	are lots of ways to invoke processes in PowerShell with Start-Process, Invoke-Expression, & and others but none account
+	well for the various streams and exit codes that an external process returns. Also, it's hard to write good tests
+	when launching external proceses.
+
+	This function ensures any errors are sent to the error stream, standard output is sent via the Output stream and any
+	time the process returns an exit code other than 0, treat it as an error.
+
+#> 
+param()
+
 function Invoke-Process {
 	[CmdletBinding(SupportsShouldProcess)]
 	param
@@ -30,16 +58,23 @@ function Invoke-Process {
 			$cmd = Start-Process @startProcessParams
 			$cmdOutput = Get-Content -Path $stdOutTempFile -Raw
 			$cmdError = Get-Content -Path $stdErrTempFile -Raw
-			if ([string]::IsNullOrEmpty($cmdOutput) -eq $false) {
-				Write-Output -InputObject $cmdOutput
-			}
 			if ($cmd.ExitCode -ne 0) {
-				throw $cmdError.Trim()
-			}	
+				if ($cmdError) {
+					throw $cmdError.Trim()
+				}
+				if ($cmdOutput) {
+					throw $cmdOutput.Trim()
+				}
+			} else {
+				if ([string]::IsNullOrEmpty($cmdOutput) -eq $false) {
+					Write-Output -InputObject $cmdOutput
+				}
+			}
 		}
 	} catch {
 		$PSCmdlet.ThrowTerminatingError($_)
 	} finally {
-		Remove-Item -Path $stdOutTempFile, $stdErrTempFile -Force
+		Remove-Item -Path $stdOutTempFile, $stdErrTempFile -Force -ErrorAction Ignore
 	}
 }
+
