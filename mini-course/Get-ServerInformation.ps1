c81678c6263f50eaba7852e2dcfa7b47cfe70334
Adam Bertram <adbertram@gmail.com>
2019-07-17 14:30:07 -0500

    Update Get-ServerInformation.ps1

--- mini-course/Get-ServerInformation.ps1
+++ mini-course/Get-ServerInformation.ps1
@@ -1,26 +1,34 @@
-    $serversOuPath = 'OU=Servers,DC=powerlab,DC=local'
-    $servers = Get-ADComputer -SearchBase $serversOuPath -Filter * | Select-Object -ExpandProperty Name
-    foreach ($server in $servers) {
-        $output = @{
-            'ServerName'                  = $null
-            'IPAddress'                   = $null
-            'OperatingSystem'             = $null
-            'AvailableDriveSpace (GB)'    = $null
-            'Memory (GB)'                 = $null
-            'UserProfilesSize (MB)'       = $null
-            'StoppedServices'             = $null
-        }
-        $getCimInstParams = @{
-            CimSession = (New-CimSession -ComputerName $server)
-        }
-        $output.ServerName = $server
-        $userProfileSize = (Get-ChildItem -Path "\\$server\c$\Users\" -File | Measure-Object -Property Length -Sum).Sum
-        $output.'UserProfilesSize (MB)' = $userProfileSize / 1GB
-        $output.'AvailableDriveSpace (GB)' = [Math]::Round(((Get-CimInstance @getCimInstParams -ClassName Win32_LogicalDisk).FreeSpace / 1GB),1)
-        $output.'OperatingSystem' = (Get-CimInstance @getCimInstParams -ClassName Win32_OperatingSystem).Caption
-        $output.'Memory (GB)' = (Get-CimInstance @getCimInstParams -ClassName Win32_PhysicalMemory | Measure-Object -Property Capacity -Sum).Sum /1GB
-        $output.'IPAddress' = (Get-CimInstance @getCimInstParams -ClassName Win32_NetworkAdapterConfiguration -Filter "IPEnabled = 'True'").IPAddress[0]
-        $output.StoppedServices = (Get-Service -ComputerName $server | Where-Object { $_.Status -eq 'Stopped' }).DisplayName
-        Remove-CimSession -CimSession $cimSession
-        [pscustomobject]$output
+$serversOuPath = 'OU=Servers,DC=powerlab,DC=local'
+$servers = Get-ADComputer -SearchBase $serversOuPath -Filter * | Select-Object -ExpandProperty Name
+foreach ($server in $servers) {
+    $output = @{
+        'ServerName'                  = $null
+        'IPAddress'                   = $null
+        'OperatingSystem'             = $null
+        'AvailableDriveSpace (GB)'    = $null
+        'Memory (GB)'                 = $null
+        'UserProfilesSize (MB)'       = $null
+        'StoppedServices'             = $null
     }
+    $getCimInstParams = @{
+        CimSession = (New-CimSession -ComputerName $server)
+    }
+    $output.ServerName = $server
+
+    $userProfileSize = (Get-ChildItem -Path "\\$server\c$\Users\" -File | Measure-Object -Property Length -Sum).Sum
+    $output.'UserProfilesSize (MB)' = $userProfileSize / 1GB
+    
+    $output.'AvailableDriveSpace (GB)' = [Math]::Round(((Get-CimInstance @getCimInstParams -ClassName Win32_LogicalDisk).FreeSpace / 1GB),1)
+    
+    $output.'OperatingSystem' = (Get-CimInstance @getCimInstParams -ClassName Win32_OperatingSystem).Caption
+
+    $output.'Memory (GB)' = (Get-CimInstance @getCimInstParams -ClassName Win32_PhysicalMemory | Measure-Object -Property Capacity -Sum).Sum /1GB
+    
+    $output.'IPAddress' = (Get-CimInstance @getCimInstParams -ClassName Win32_NetworkAdapterConfiguration -Filter "IPEnabled = 'True'").IPAddress[0]
+    
+    $output.StoppedServices = (Get-Service -ComputerName $server | Where-Object { $_.Status -eq 'Stopped' }).DisplayName
+
+    Remove-CimSession @getCimInstParams
+    
+    [pscustomobject]$output
+}
