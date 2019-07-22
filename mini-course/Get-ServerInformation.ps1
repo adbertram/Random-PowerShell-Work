d69d4e8dad6700678b1384c7a171ec022f9e27a6
Adam Bertram <adbertram@gmail.com>
2019-07-22 13:52:17 -0500

    Update Get-ServerInformation.ps1

--- mini-course/Get-ServerInformation.ps1
+++ mini-course/Get-ServerInformation.ps1
@@ -1,14 +1,33 @@
 $serversOuPath = 'OU=Servers,DC=powerlab,DC=local'
-$servers = Get-ADComputer -SearchBase $serversOuPath -Filter * | Select-Object -ExpandProperty Name
-foreach ($server in $servers) {
+$servers = Get-ADComputer -SearchBase $serversOuPath -Filter * |
+Select-Object -ExpandProperty Name
+
+$Header = @"
+<style>
+    table {
+        font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+        border-collapse: collapse;
+        width: 100%;
+    }
+    th {
+        padding-top: 12px;
+        padding-bottom: 12px;
+        text-align: left;
+        background-color: #4CAF50;
+        color: white;
+    }
+</style>
+"@
+
+$report = foreach ($server in $servers) {
     $output = [ordered]@{
-        'ServerName'                  = $null
-        'IPAddress'                   = $null
-        'OperatingSystem'             = $null
-        'AvailableDriveSpace (GB)'    = $null
-        'Memory (GB)'                 = $null
-        'UserProfilesSize (MB)'       = $null
-        'StoppedServices'             = $null
+        'ServerName'               = $null
+        'IPAddress'                = $null
+        'OperatingSystem'          = $null
+        'AvailableDriveSpace (GB)' = $null
+        'Memory (GB)'              = $null
+        'UserProfilesSize (MB)'    = $null
+        'StoppedServices'          = $null
     }
     $getCimInstParams = @{
         CimSession = (New-CimSession -ComputerName $server)
@@ -18,7 +37,7 @@ foreach ($server in $servers) {
     $userProfileSize = (Get-ChildItem -Path "\\$server\c$\Users\" -File -Recurse | Measure-Object -Property Length -Sum).Sum
     $output.'UserProfilesSize (MB)' = $userProfileSize / 1GB
     
-    $output.'AvailableDriveSpace (GB)' = Get-CimInstance @getCimInstParams -ClassName Win32_LogicalDisk | Select-Object -Property DeviceID,@{Name='FreeSpace';Expression={ [Math]::Round(($_.Freespace / 1GB),1) }}
+    $output.'AvailableDriveSpace (GB)' = Get-CimInstance @getCimInstParams -ClassName Win32_LogicalDisk | Select-Object -Property DeviceID, @{Name='FreeSpace'; Expression={ [Math]::Round(($_.Freespace / 1GB), 1) } }
     
     $output.'OperatingSystem' = (Get-CimInstance @getCimInstParams -ClassName Win32_OperatingSystem).Caption
 
@@ -32,3 +51,6 @@ foreach ($server in $servers) {
     
     [pscustomobject]$output
 }
+
+$reportfinal = $report | ConvertTo-Html -Fragment
+ConvertTo-HTML -PreContent "<h1>Server Information Report</h1>"  -PostContent $reportFINAL -Head $Header | Out-File ServerReport.html
