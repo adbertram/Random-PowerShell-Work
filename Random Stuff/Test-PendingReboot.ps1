cdfc7a65a6de6a628938994b3c6ab2cb1492e3b3
Adam Bertram <adbertram@gmail.com>
2019-08-03 13:54:16 -0500

    test-pendingreboot

--- Random Stuff/Test-PendingReboot.ps1
+++ Random Stuff/Test-PendingReboot.ps1
@@ -1,57 +1,184 @@
-function Test-PendingReboot {
-	<#
-		.SYNOPSIS
-			This function tests various registry values to see if the local computer is pending a reboot
-		.NOTES
-			Inspiration from: https://gallery.technet.microsoft.com/scriptcenter/Get-PendingReboot-Query-bdb79542
-		.EXAMPLE
-			PS> Test-PendingReboot
-			
-			This example checks various registry values to see if the local computer is pending a reboot.
-	#>
-	[CmdletBinding()]
-	param
-	(
-		[Parameter(Mandatory)]
-		[ValidateNotNullOrEmpty()]
-		[string]$ComputerName
-	)
-	begin {
-		$ErrorActionPreference = 'Stop'
-	}
-	process {
-		try {
-			$icmParams = @{
-				'ComputerName' = $ComputerName
-			}
-			
-			$OperatingSystem = Invoke-Command @icmParams -ScriptBlock { Get-CimInstance -ClassName 'Win32_OperatingSystem' -Property 'BuildNumber', 'CSName' }
 
-			# If Vista/2008 & Above query the CBS Reg Key
-			If ($OperatingSystem.BuildNumber -ge 6001) {
-				$icmParams.ScriptBlock = { Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Component Based Servicing' -Name 'RebootPending' -ErrorAction SilentlyContinue }
-				if (Invoke-Command @icmParams) {
-					Write-Verbose -Message 'Reboot pending detected in the Component Based Servicing registry key'
-					$true
-				}
-			}
+<#PSScriptInfo
 
-			# Query WUAU from the registry
-			$icmParams.ScriptBlock = { Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update' -Name 'RebootRequired' -ErrorAction SilentlyContinue }
-			if (Invoke-Command @icmParams) {
-				Write-Verbose -Message 'WUAU has a reboot pending'
-				$true
-			}
-			
-			# Query PendingFileRenameOperations from the registry
-			$icmParams.ScriptBlock = { Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager' -Name 'PendingFileRenameOperations' -ErrorAction SilentlyContinue }
-			$PendingReboot = Invoke-Command @icmParams
-			if ($PendingReboot -and $PendingReboot.PendingFileRenameOperations) {
-				Write-Verbose -Message 'Reboot pending in the PendingFileRenameOperations registry value'
-				$true
-			}
-		} catch {
-			$PSCmdlet.ThrowTerminatingError($_)
-		}
-	}
+.VERSION 1.5
+
+.GUID fe3d3698-52fc-40e8-a95c-bbc67a507ed1
+
+.AUTHOR Adam Bertram
+
+.COMPANYNAME Adam the Automator, LLC
+
+.COPYRIGHT 
+
+.DESCRIPTION This function tests various registry values to see if the local computer is pending a reboot.
+
+.TAGS 
+
+.LICENSEURI 
+
+.PROJECTURI 
+
+.ICONURI 
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS 
+
+.EXTERNALSCRIPTDEPENDENCIES 
+
+.RELEASENOTES
+
+.SYNOPSIS
+	This function tests various registry values to see if the local computer is pending a reboot
+.NOTES
+	Inspiration from: https://gallery.technet.microsoft.com/scriptcenter/Get-PendingReboot-Query-bdb79542
+.EXAMPLE
+	PS> Test-PendingReboot
+	
+	This example checks various registry values to see if the local computer is pending a reboot.
+#>
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string[]]$ComputerName,
+	
+    [Parameter()]
+    [ValidateNotNullOrEmpty()]
+    [pscredential]$Credential
+)
+begin {
+    $ErrorActionPreference = 'Stop'
+
+    function Test-RegistryKey {
+        [OutputType('bool')]
+        [CmdletBinding()]
+        param
+        (
+            [Parameter(Mandatory)]
+            [ValidateNotNullOrEmpty()]
+            [string]$Key,
+
+            [Parameter(Mandatory)]
+            [ValidateNotNullOrEmpty()]
+            [System.Management.Automation.Runspaces.PSSession]$Session
+        )
+    
+        $ErrorActionPreference = 'Stop'
+
+        Invoke-Command -Session $Session -ScriptBlock {
+            if (Get-Item -Path $using:Key -ErrorAction Ignore) {
+                $true
+            }
+        }
+    }
+
+    function Test-RegistryValue {
+        [OutputType('bool')]
+        [CmdletBinding()]
+        param
+        (
+            [Parameter(Mandatory)]
+            [ValidateNotNullOrEmpty()]
+            [System.Management.Automation.Runspaces.PSSession]$Session,
+
+            [Parameter(Mandatory)]
+            [ValidateNotNullOrEmpty()]
+            [string]$Key,
+
+            [Parameter(Mandatory)]
+            [ValidateNotNullOrEmpty()]
+            [string]$Value
+        )
+    
+        $ErrorActionPreference = 'Stop'
+
+        Invoke-Command -Session $Session -ScriptBlock {
+            if (Get-ItemProperty -Path $using:Key -Name $using:Value -ErrorAction Ignore) {
+                $true
+            }
+        }
+    }
+
+    function Test-RegistryValueNotNull {
+        [OutputType('bool')]
+        [CmdletBinding()]
+        param
+        (
+            [Parameter(Mandatory)]
+            [ValidateNotNullOrEmpty()]
+            [System.Management.Automation.Runspaces.PSSession]$Session,
+
+            [Parameter(Mandatory)]
+            [ValidateNotNullOrEmpty()]
+            [string]$Key,
+
+            [Parameter(Mandatory)]
+            [ValidateNotNullOrEmpty()]
+            [string]$Value
+        )
+    
+        $ErrorActionPreference = 'Stop'
+
+        Invoke-Command -Session $Session -ScriptBlock {
+            if (($regVal = Get-ItemProperty -Path $using:Key -Name $using:Value -ErrorAction Ignore) -and $regVal.($using:Value)) {
+                $true
+            }
+        }
+    }
+
+    $tests = @(
+        { Test-RegistryKey -Key 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending' }
+        { Test-RegistryKey -Key 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootInProgress' }
+        { Test-RegistryKey -Key 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired' }
+        { Test-RegistryKey -Key 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing\PackagesPending' }
+        { Test-RegistryKey -Key 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\PostRebootReporting' }
+        { Test-RegistryValueNotNull -Key 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager' -Value 'PendingFileRenameOperations' }
+        { Test-RegistryValueNotNull -Key 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager' -Value 'PendingFileRenameOperations2' }
+        { (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Updates' -Name 'UpdateExeVolatile' -ErrorAction Ignore | Select-Object -ExpandProperty UpdateExeVolatile) -ne 0 }
+        { Test-RegistryValue -Key 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce' -Value 'DVDRebootSignal' }
+        { Test-RegistryKey -Key 'HKLM:\SOFTWARE\Microsoft\ServerManager\CurrentRebootAttemps' }
+        { Test-RegistryValue -Key 'HKLM:\SYSTEM\CurrentControlSet\Services\Netlogon' -Value 'JoinDomain' }
+        { Test-RegistryValue -Key 'HKLM:\SYSTEM\CurrentControlSet\Services\Netlogon' -Value 'AvoidSpnSet' }
+        {
+            (Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\ComputerName' -Name 'ActiveComputerName').ActiveComputerName -ne
+            (Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\ComputerName' -Name 'ActiveComputerName').ComputerName
+        }
+        {
+            if (Get-ChildItem -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Services\Pending') {
+                $true
+            }
+        }
+    )
+}
+process {
+    try {
+        foreach ($computer in $ComputerName) {
+            $connParams = @{
+                'ComputerName' = $computer
+            }
+            if ($PSBoundParameters.ContainsKey('Credential')) {
+                $connParams.Credential = $Credential
+            }
+
+            $output = @{
+                ComputerName    = $computer
+                IsPendingReboot = $false
+            }
+
+            $psRemotingSession = New-PSSession @connParams
+            
+            foreach ($test in $tests) {
+                if (& $test) {
+                    $output.IsPendingReboot = $true
+                }
+                [pscustomobject]$output
+            }
+        }
+    } catch {
+        Write-Error -Message $_.Exception.Message
+    } finally {
+        $psRemotingSession | Remove-PSSession
+    }
 }
