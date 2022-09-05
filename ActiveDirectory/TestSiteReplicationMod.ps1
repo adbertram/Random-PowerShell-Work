a5bff5819623d7984666a0b9c8339af5f7062239
Adam Bertram <adbertram@gmail.com>
2022-09-05 15:54:03 -0500

    Update TestSiteReplicationMod.ps1

--- ActiveDirectory/TestSiteReplicationMod.ps1
+++ ActiveDirectory/TestSiteReplicationMod.ps1
@@ -1,9 +1,9 @@
-﻿echo "Starting UAP --> POB replication test..."
+﻿echo "Starting replication test..."
 echo "-----------"
-#Set-AdUser abertram -Server UAPDC01 -Description "Keller Schroeder Vendor - Set on UAPDC01"
-Set-ADAccountPassword abertramtest -NewPassword (ConvertTo-SecureString 'p@$$w0rd14' -AsPlainText -Force) -Reset -Server UAPDC01
-$passwordlastset = (Get-Aduser abertramtest -Properties passwordlastset -Server UAPDC01).passwordlastset
-Write-Host "Waiting for replication from UAPDC01 to DC01..." -ForegroundColor Yellow
+#Set-AdUser abertram -Server xxxx -Description "xxxxx"
+Set-ADAccountPassword abertramtest -NewPassword (ConvertTo-SecureString 'p@$$w0rd14' -AsPlainText -Force) -Reset -Server XXXX
+$passwordlastset = (Get-Aduser abertramtest -Properties passwordlastset -Server XXXX).passwordlastset
+Write-Host "Waiting for replication..." -ForegroundColor Yellow
 $i = 0
 do { 
     $i++
@@ -11,29 +11,27 @@ do {
 #} while ((Get-AdUser abertram -Properties description -Server DC01).description -ne "Keller Schroeder Vendor - Set on UAPDC01")
 } while ((Get-Aduser abertramtest -Properties passwordlastset -Server DC01).passwordlastset -ne $passwordlastset)
 
-Write-Host "Replication from DC01 from UAPDC01 successful.  Replication time: $i seconds ($($i / 60) minutes)" -ForegroundColor Green
+Write-Host "Replication from XXXX from XXXX successful.  Replication time: $i seconds ($($i / 60) minutes)" -ForegroundColor Green
 
 
-echo "Starting POB --> UAP replication test..."
+echo "Starting replication test..."
 echo "-----------"
-#Set-AdUser abertram -Server DC01 -Description "Keller Schroeder Vendor - Set on DC01"
-Write-Host "Waiting for replication from DC01 to UAPDC01..." -ForegroundColor Yellow
-Set-ADAccountPassword abertramtest -NewPassword (ConvertTo-SecureString 'p@$$w0rd15' -AsPlainText -Force) -Reset -Server DC01
-$passwordlastset = (Get-Aduser abertramtest -Properties passwordlastset -Server DC01).passwordlastset
+Write-Host "Waiting for replication from XXXXX to XXXX..." -ForegroundColor Yellow
+Set-ADAccountPassword XXXXX -NewPassword (ConvertTo-SecureString 'p@$$w0rd15' -AsPlainText -Force) -Reset -Server XXXX
+$passwordlastset = (Get-Aduser XXXX -Properties passwordlastset -Server DC01).passwordlastset
 $i = 0
 do { 
     $i++
     sleep 1
-#} while ((Get-AdUser abertram -Properties description -Server UAPDC01).description -ne "Keller Schroeder Vendor - Set on DC01")
-} while ((Get-Aduser abertramtest -Properties passwordlastset -Server UAPDC01).passwordlastset -ne $passwordlastset)
+} while ((Get-Aduser XXXXX -Properties passwordlastset -Server XXXXX).passwordlastset -ne $passwordlastset)
 
-Write-Host "Replication from UAPDC01 from DC01 successful. Replication time: $i seconds ($($i / 60) minutes)" -ForegroundColor Green
+Write-Host "Replication from XXXX from XXXXX successful. Replication time: $i seconds ($($i / 60) minutes)" -ForegroundColor Green
 
 
-Set-AdUser abertram -Server DC01 -Description "Keller Schroeder Vendor"
+Set-AdUser XXXXX -Server XXXX -Description "XXXXXX"
 
 echo '-----------'
-echo 'Checking last replication status between UHHG and UHC sites...'
+echo 'Checking last replication status between XXXX and XXXX sites...'
 echo "-----------"
-Get-ADReplicationLink -SiteName uhhg | ? { $_.sourceserver -eq 'DC1' } | select sourceserver,destinationserver,LastSuccessfulsync,lastsyncmessage
-Get-ADReplicationLink -SiteName UAPMain | ? { $_.sourceserver -eq 'DC02' } | select sourceserver,destinationserver,LastSuccessfulsync,lastsyncmessage
+Get-ADReplicationLink -SiteName xxxxx | ? { $_.sourceserver -eq 'XXXX' } | select sourceserver,destinationserver,LastSuccessfulsync,lastsyncmessage
+Get-ADReplicationLink -SiteName xxxx | ? { $_.sourceserver -eq 'XXXX' } | select sourceserver,destinationserver,LastSuccessfulsync,lastsyncmessage
